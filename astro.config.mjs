2d4ed87b061d6f87f3829603ffcd12257de44278
Angad Singh Kochhar <kochhara@purdue.edu>
2026-08-14 18:12:30 -0400

    Name Change to match GitHub, Deployment Success.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://angadq.github.io',
-  base: '/angad-kochhar.github.io',
+  //base: '/angad-kochhar.github.io',
+  //base is commented as Repo name is same as GitHub username, so no need to add base path
+  //GitHub username - AngadQ
+  //Repo name - AngadQ
+  //Website will be AngadQ.github.io
 });
