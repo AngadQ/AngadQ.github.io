59df22ac966387eac38e88f20b22b106e7cde93b
Angad Singh Kochhar <kochhara@purdue.edu>
2026-08-14 17:54:15 -0400

    Deployement fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  // ... your existing config (markdown, vite, integrations, etc.)
-
-  site: 'https://angad-kochhar.github.io',   // ← change this
-  base: '/portfolio',                   // ← only if repo is NOT username.github.io
+  site: 'https://angadq.github.io',
+  base: '/angad-kochhar.github.io',
 });
